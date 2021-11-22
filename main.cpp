8295898a9fbd70fc54196d1129885e2d2226239b
zzttzz <2775859312@qq.com>
2021-11-22 15:16:55 +0800

    适配VS编译器

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
+#if defined(_MSC_VER) && (_MSC_VER >= 1600)
+#pragma execution_character_set("utf-8")
+#endif
+
 #include "widget.h"
 
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    Widget w;
-    w.show();
-    return a.exec();
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
+  //设置中文字体
+  // a.setFont(QFont("Microsoft Yahei", 9));
+
+  //设置中文编码
+#if (QT_VERSION <= QT_VERSION_CHECK(5, 0, 0))
+#if _MSC_VER
+  QTextCodec *codec = QTextCodec::codecForName("gbk");
+#else
+  QTextCodec *codec = QTextCodec::codecForName("utf-8");
+#endif
+  QTextCodec::setCodecForLocale(codec);
+  QTextCodec::setCodecForCStrings(codec);
+  QTextCodec::setCodecForTr(codec);
+#else
+  QTextCodec *codec = QTextCodec::codecForName("utf-8");
+  QTextCodec::setCodecForLocale(codec);
+#endif
+  Widget w;
+  w.show();
+  return a.exec();
 }
